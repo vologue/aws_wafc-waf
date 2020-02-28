2cb59d71624c243e1b4d7a43c8c2a4e57f785c62
raghav_zolo <raghav.p@zolostays.com>
2020-02-28 17:25:16 +0530

    fixed - not able to run from script

--- create_web_acl.sh
+++
@@ -1,8 +0,0 @@
-aws wafv2 create-web-acl --name $1 \
---scope REGIONAL \
---rules file://rulegroup.json \
---default-action Block={} \
---visibility-config $2 \
---region ap-south-1  
-
-# --capacity 1500 
--- waf-classic-to-associate.py
+++ waf-classic-to-associate.py
@@ -13,7 +13,7 @@ val  = json.loads(os.popen('aws waf-regional list-web-acls').read())
 for acl in val['WebACLs']:
     acl_id = acl['WebACLId']
     acl_desc = json.loads(os.popen('aws waf-regional get-web-acl --web-acl-id %s' %(acl_id)).read())['WebACL']
-    def_action = acl_desc['DefaultAction']['Type']
+    def_action = acl_desc['DefaultAction']['Type'][0]+acl_desc['DefaultAction']['Type'][1:].lower()
     name = removespace(acl_desc['Name'])+'Rule'
     visibilityconfig = "SampledRequestsEnabled=true,CloudWatchMetricsEnabled=true,MetricName="+name+"METRICS"
     region = "ap-south-1"
@@ -31,16 +31,15 @@ for acl in val['WebACLs']:
             rules.append(newrule)
     print("Generated Json file for creating the group - ")
     rules = json.dumps(rules)
-    print(rules)
+    # print(rules)
     f = open("rulegroup.json","w")
     f.write(rules)
     f.close()
     print("Please run the following command -")
-    # For some reason this refuses to run from here
-    command  = "./create_web_acl.sh %s %s" %(name,visibilityconfig)
+    command  = "aws wafv2 create-web-acl --name %s --scope REGIONAL --default-action %s={} --rules file://rulegroup.json --visibility-config %s" %(name,def_action,visibilityconfig)
     print(command)
-    #aclv2 = json.loads(os.popen(command).read())
-    #aclv2 = aclv2["Summary"]["ARN"]
+    aclv2 = json.loads(os.popen(command).read())
+    aclv2 = aclv2["Summary"]["ARN"]
     aclv2 = input("Enter ARN of the new Web ACL :")
     arns = associate_rescource.get_rescource_list("2c659f1c-8f9b-40fd-834a-89961e7895eb")
     associate_rescource.disassociate_rescource(arns=arns)
